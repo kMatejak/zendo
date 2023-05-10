4cf8b7d6441ffc5cdb84dd60f7293c055cdbc8f3
Krzysztof S. Matejak <krzysztof@matejak.com>
2023-05-10 18:29:20 +0200

    add quote interface

--- zendo-app/src/app/app.component.ts
+++ zendo-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'zendo-app';
+export class AppComponent implements OnInit {
+  quote = 'zendo';
+
+  // quote: Quote
+
+  constructor(
+  ) {}
+
+  ngOnInit(): void {
+  }
 }
